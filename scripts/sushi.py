ebdb048f50f1347267add4559d52adfa2c9f6dbe
Siya Kesarwani <109158101+SiyaKesarwani@users.noreply.github.com>
2022-08-14 11:14:55 +0530

    Update sushi.py

--- scripts/sushi.py
+++ scripts/sushi.py
@@ -1,9 +1,14 @@
 #!/usr/bin/python3
 
-from brownie import SushiToken, Sushibar, accounts
+import brownie
+from brownie import SushiToken, Sushibar
+
+
+accounts = brownie.accounts
+pool = "0xb223EACc07D4043A14B22b6a3BF9920EF4d3Db67"
 
 
 def main():
-    return SushiToken.deploy({'from': accounts[0], 'gas_limit': 100000000})
-
+    token = SushiToken.deploy({'from': accounts[0], 'gas_price': 1000000000})
+    sushibar = Sushibar.deploy(token.address, pool, {'from': accounts[0], 'gas_price': 1000000000})
 
